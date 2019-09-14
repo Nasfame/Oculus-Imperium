0ddf533a35f21a29114879bb609af6fef1f9963d
Hiro Hamada <laciferin@gmail.com>
2019-09-14 15:04:25 +0530

    Oculus 2.0
    More levels of questions

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function showProgress() {
 };
 
 function showScores() {
-    var gameOverHTML = "<h1>How good are your eyes? </h1> <img src = 'table.png'> </img>";
+    var gameOverHTML = "<h1>How good are your eyes? </h1> <img src = 'table.png'width:200px> </img>";
     gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
     var element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
@@ -63,7 +63,17 @@ var questions = [
     new Question(document.getElementById("magic").innerHTML = "<img src='level7.png'>", ["10", "3","7", "4"], "10"),
     new Question(document.getElementById("magic").innerHTML = "<img src='level8.png'>", ["4", "8","2", "7"], "2"),
     new Question(document.getElementById("magic").innerHTML = "<img src='level9.png'>", ["1", "5","10", "9"], "9"),
-    new Question(document.getElementById("magic").innerHTML = "<img src='level10.png'>", ["1", "8","3", "4"], "1"),
+    new Question(document.getElementById("magic").innerHTML = "<img src='level10.png'>", ["1", "8","10", "4"], "1"),
+	new Question(document.getElementById("magic").innerHTML = "<img src='level11.png'>", ["2", "6","5", "7"], "5"),
+	new Question(document.getElementById("magic").innerHTML = "<img src='level12.png'>", ["3", "8","5", "4"], "5"),
+	new Question(document.getElementById("magic").innerHTML = "<img src='level13.png'>", ["2", "9","10", "4"], "9"),
+	new Question(document.getElementById("magic").innerHTML = "<img src='level14.png'>", ["1", "8","2", "4"], "1"),
+	new Question(document.getElementById("magic").innerHTML = "<img src='level15.png'>", ["2", "1","6", "4"], "2"),
+	new Question(document.getElementById("magic").innerHTML = "<img src='level16.png'>", ["6", "9","1", "4"], "1"),
+	new Question(document.getElementById("magic").innerHTML = "<img src='level17.png'>", ["2", "8","6", "4"], "4"),
+	new Question(document.getElementById("magic").innerHTML = "<img src='level18.png'>", ["1", "8","7", "5"], "8"),
+	new Question(document.getElementById("magic").innerHTML = "<img src='level19.png'>", ["9", "6","2", "7"], "7"),
+	new Question(document.getElementById("magic").innerHTML = "<img src='level20.png'>", ["3", "4","2", "8"], "2"),
 ];
 
 // create quiz
